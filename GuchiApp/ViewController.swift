b43eb2a965d8e19b2cae52d686f04ea08abf10c4
honda-nonoho <nnhhnd3012@gmail.com>
2020-05-11 17:20:15 +0900

    Initial Commit

---
+++ GuchiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuchiApp
+//
+//  Created by nonoho.honda on 2020/05/11.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
